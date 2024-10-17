ce610f2fe13704a606781db108afd9c8e7055155
Kristian Herud <kristianherud11@gmail.com>
2024-10-17 20:08:09 +0200

    oprava chyby

--- app/components/main/About.tsx
+++ app/components/main/About.tsx
@@ -7,18 +7,22 @@ const About = () => {
       <div className="flex p-5 flex-col gap-3 justify-center items-center text-center bg-neutral-500">
         <UtensilsCrossedIcon className="w-24 h-24" />
         <p className="font-medium text-xl ">
-         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro provident commodi
-      </div>
-      <div className="flex p-5 flex-col gap-3 justify-center items-center text-center bg-neutral-500">
-        <UtensilsCrossedIcon className="w-24 h-24" />
-        <p className="font-medium text-xl ">
-           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro provident commodi
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro
+          provident commodi
         </p>
       </div>
       <div className="flex p-5 flex-col gap-3 justify-center items-center text-center bg-neutral-500">
         <UtensilsCrossedIcon className="w-24 h-24" />
         <p className="font-medium text-xl ">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro provident commodi
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro
+          provident commodi
+        </p>
+      </div>
+      <div className="flex p-5 flex-col gap-3 justify-center items-center text-center bg-neutral-500">
+        <UtensilsCrossedIcon className="w-24 h-24" />
+        <p className="font-medium text-xl ">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro
+          provident commodi
         </p>
       </div>
     </div>
